bbb122ba3a29b9f74002a49fa01b61dc6b555255
23f2002152-IITM <23f2002152@ds.study.iitm.ac.in>
2025-05-23 01:47:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json
+
+app = Flask(__name__)
+
+marks_db = json.load(open('q-vercel-python.json'))
+
+@app.route('/api',methods=['GET'])
+def index():
+    names = request.args.getlist('name')
+    results = []
+    for i in marks_db:
+        if i['name'] in names:
+            results.append(i['marks'])
+    return jsonify({'marks':results})
+    # return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
