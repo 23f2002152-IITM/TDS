89f34af4a3483bfe3be6f6c4476523d4730de59d
23f2002152-IITM <23f2002152@ds.study.iitm.ac.in>
2025-05-23 02:02:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ CORS(app)
 
 marks_db = json.load(open('q-vercel-python.json'))
 
-@app.route('/api',methods=['GET'])
+@app.route('/api', methods=['GET'])
 def index():
     names = request.args.getlist('name')
-    results = []
-    for i in marks_db:
-        if i['name'] in names:
-            results.append(i['marks'])
-    return jsonify({'marks':results})
+    name_to_marks = {entry['name']: entry['marks'] for entry in marks_db}
+    results = [name_to_marks[name] for name in names if name in name_to_marks]
+    return jsonify({'marks': results})
+
     # return render_template('index.html')
 
 if __name__ == '__main__':
